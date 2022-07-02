fe7c54e2c692bf13c4b0c2a5ce95f390005c07f2
acerohernan <contacto.acero.hernan@gmail.com>
2022-07-02 08:29:33 -0500

    Database connection update

--- database/data-source.ts
+++ database/data-source.ts
@@ -6,8 +6,16 @@ import { User } from '../models/user/user.model';
 export const sequelize = new Sequelize({
   database: config.get<string>('database.name'),
   dialect: config.get<'postgres'>('database.type'),
-  username: config.get<'postgres'>('database.username'),
-  password: config.get<'postgres'>('database.password'),
+  username: config.get<string>('database.username'),
+  password: config.get<string>('database.password'),
+  host: config.get<string>('database.host'),
   models: [User, Store],
   storage: ':memory:',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
